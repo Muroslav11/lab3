5176ff4d138beb25b17d3d85200fc245034c98a4
Muroslav11 <Muroslavhalik@gmail.com>
2025-10-05 17:21:12 +0300

    lab 3.3

---
+++ lab3.3/lab3.3/lab3.3.cpp
@@ -0,0 +1,37 @@
+﻿// Lab_03_3.cpp
+// < прізвище, ім’я автора >
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 0.1
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double R; // вхідний параметр
+	double y; // результат обчислення виразу
+
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+
+	// розгалуження в повній формі
+	if (x <= -8 - R)
+		y = R;
+	else
+		if (-8 - R < x && x <= -8 + R)
+			y = R - sqrt(R * R - x * x - 16 * x - 64);
+		else
+			if (-8 + R < x && x <= -4)
+				y = R;
+			else
+				if (-4 < x && x <= 2)
+					y = -x - 4 - R * x + 2 * R;
+				else
+					y = x - 3;
+	cout << endl;
+	cout << "y = " << y << endl;
+	cin.get();
+	return 0;
+}
