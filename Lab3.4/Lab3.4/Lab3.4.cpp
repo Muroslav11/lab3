a4c5620d76f59169a74f38a77f121c4a48385600
Muroslav11 <Muroslavhalik@gmail.com>
2025-10-05 21:15:45 +0300

    lab3.4

---
+++ Lab3.4/Lab3.4/Lab3.4.cpp
@@ -0,0 +1,34 @@
+﻿// Lab_03_4.cpp
+// Галика Мирослава
+// Лабораторна робота №3.4
+// Розгалуження, задане плоскою фігурою
+// Варіант 10
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double x;  // вхідний аргумент
+    double y;  // вхідний параметр
+    double R;  // вхідний параметр — радіус кола
+    double a;  // вхідний параметр — половина ширини прямокутника
+    double b;  // вхідний параметр — половина висоти прямокутника
+
+    cout << "x = "; cin >> x;
+    cout << "y = "; cin >> y;
+    cout << "R = "; cin >> R;
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+
+    // розгалуження в повній формі
+    if ((x * x + y * y <= R * R && x >= -a && x <= 0 && y >= -b && y <= 0) || 
+        (x * x + y * y >= R * R && x >= 0 && x <= a && y >= 0 && y <= b))
+        cout << "yes" << endl;
+    else
+        cout << "no" << endl;
+
+    cin.get();
+    return 0;
+}
